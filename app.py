2f76051243bf7a5ee6ebdeffa47f81ce4e9bf9bb
Scott Leerssen <scott.leerssen@candidpartners.com>
2018-11-04 12:37:02 -0500

    wip

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask import request
+import json
+import pprint
+
 app = Flask(__name__)
 
 @app.route("/pyatl", methods=['POST', 'GET'])
 def pyatl():
     print(request.args)
     print(request.data)
+    pprint.pprint(json.loads(request.data))
     print(request.form)
     return "Hello World!"
 
