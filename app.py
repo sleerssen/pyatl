24575a0b8043d690b70ca8231fceb8a78038293e
Scott Leerssen <scott.leerssen@candidpartners.com>
2018-11-04 12:26:59 -0500

    wip

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route("/pyatl", methods=['POST', 'GET'])
 def pyatl():
     print(request.args)
+    print(request.data)
     print(request.form)
     return "Hello World!"
 
