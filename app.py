789f310c9c41e2935498b47c3690a45e888c69bc
Scott Leerssen <scott.leerssen@candidpartners.com>
2018-11-06 17:29:50 -0500

    ignore None parts

--- app.py
+++ app.py
@@ -40,6 +40,8 @@ def pyatl():
             with open(t.name, 'rb') as f:
                 m = email.message_from_binary_file(f, policy=default)
             for p in m.walk():
+                if not p:
+                    continue
                 print('found mime part {}'.format(p.get_filename()))
                 payload = p.get_payload(decode=True)
                 print(payload)
